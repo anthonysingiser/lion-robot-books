a2a4ae3a29b036256337bfec7dc2a3eeea33454e
Anthony <asingiser@gmail.com>
2024-02-12 09:06:52 -0500

    test incorporating lists into the prompt

--- openai-test.mjs
+++ openai-test.mjs
@@ -1,17 +1,21 @@
 import 'dotenv/config';
 import OpenAI from 'openai';
+import {animals, sightWords} from './story-details.js';
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+const oneRandomAnimal = animals[Math.round(Math.random(animals.length))]
+const wordsToInclude = sightWords.join(',')
+
 async function main() {
     const completion = await openai.chat.completions.create({
-        messages: [{ role: 'user', content: 'Say this is a test.'}],
+        messages: [{ role: 'user', content: `Write me a story about a ${oneRandomAnimal} in 15 sentences. Incorporate as many of the following words as possible: ${wordsToInclude}`}],
         model: 'gpt-3.5-turbo',
     });
 
-    console.log(completion.choices[0]);
+    console.log(completion.choices[0].message.content);
 }
 
-main();
+main()
---
+++ story-details.js
@@ -0,0 +1,63 @@
+// list of animals to have as the subject of a story, to be chosen or randomly selected
+
+const animals = [
+    'unicorn',
+    'chipmunk',
+    'squirrel',
+    'dog',
+    'kitten',
+]
+
+// list of sight words that will be included in the story, 
+// the goal here should be to include multiple lists by grade level
+// that can be selected by the user. An alternative would be to use lists of words 
+// that are hard and easy for the user, and incorporate both.
+// Another alternative will be to select by phonetics, long a, short a, etc.   
+const sightWords = [
+    'after', 
+    'again', 
+    'an', 
+    'any', 
+    'as', 
+    'ask', 
+    'by', 
+    'could', 
+    'every', 
+    'fly', 
+    'from', 
+    'give',
+    'going', 
+    'had', 
+    'has', 
+    'her', 
+    'him', 
+    'his', 
+    'how', 
+    'just', 
+    'know', 
+    'let', 
+    'live', 
+    'may', 
+    'of', 
+    'old', 
+    'once', 
+    'open', 
+    'over', 
+    'put', 
+    'round', 
+    'some', 
+    'stop', 
+    'take', 
+    'thank', 
+    'them', 
+    'then', 
+    'think', 
+    'walk', 
+    'were', 
+    'when'
+]
+
+module.exports = {
+    animals,
+    sightWords
+}
